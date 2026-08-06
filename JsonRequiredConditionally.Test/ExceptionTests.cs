568bcd0a92a0312331f31db64ce767bd04161596
Matt Edmondson <matthew.edmondson@gmail.com>
2026-08-06 13:19:00 +1000

    [patch] Add JsonRequiredConditionallyException

---
+++ JsonRequiredConditionally.Test/ExceptionTests.cs
@@ -0,0 +1,63 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
+namespace ktsu.JsonRequiredConditionally.Tests;
+
+using System.Text.Json;
+
+[TestClass]
+public class ExceptionTests
+{
+	[TestMethod]
+	public void ExceptionDerivesFromJsonException()
+	{
+		JsonRequiredConditionallyException exception = new(["tuning"]);
+
+		Assert.IsInstanceOfType<JsonException>(exception);
+	}
+
+	[TestMethod]
+	public void ExceptionExposesMissingProperties()
+	{
+		JsonRequiredConditionallyException exception = new(["tuning", "host"]);
+
+		CollectionAssert.AreEqual(new List<string> { "tuning", "host" }, exception.MissingProperties.ToList());
+	}
+
+	[TestMethod]
+	public void ExceptionMessageNamesEveryMissingProperty()
+	{
+		JsonRequiredConditionallyException exception = new(["tuning", "host"]);
+
+		StringAssert.Contains(exception.Message, "tuning");
+		StringAssert.Contains(exception.Message, "host");
+	}
+
+	[TestMethod]
+	public void DefaultConstructorProducesEmptyMissingProperties()
+	{
+		JsonRequiredConditionallyException exception = new();
+
+		Assert.IsEmpty(exception.MissingProperties);
+	}
+
+	[TestMethod]
+	public void MessageConstructorPreservesMessage()
+	{
+		JsonRequiredConditionallyException exception = new("custom message");
+
+		Assert.AreEqual("custom message", exception.Message);
+		Assert.IsEmpty(exception.MissingProperties);
+	}
+
+	[TestMethod]
+	public void InnerExceptionConstructorPreservesBoth()
+	{
+		InvalidOperationException inner = new("inner");
+		JsonRequiredConditionallyException exception = new("outer", inner);
+
+		Assert.AreEqual("outer", exception.Message);
+		Assert.AreSame(inner, exception.InnerException);
+	}
+}
---
+++ JsonRequiredConditionally/JsonRequiredConditionallyException.cs
@@ -0,0 +1,56 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
+namespace ktsu.JsonRequiredConditionally;
+
+using System.Text.Json;
+
+/// <summary>
+/// Thrown when one or more properties were required by their sibling values but were absent
+/// from the JSON payload.
+/// </summary>
+public sealed class JsonRequiredConditionallyException : JsonException
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonRequiredConditionallyException"/> class.
+	/// </summary>
+	public JsonRequiredConditionallyException()
+		: base() => MissingProperties = [];
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonRequiredConditionallyException"/> class.
+	/// </summary>
+	/// <param name="message">The message that describes the error.</param>
+	public JsonRequiredConditionallyException(string message)
+		: base(message) => MissingProperties = [];
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonRequiredConditionallyException"/> class.
+	/// </summary>
+	/// <param name="message">The message that describes the error.</param>
+	/// <param name="innerException">The exception that caused this exception.</param>
+	public JsonRequiredConditionallyException(string message, Exception innerException)
+		: base(message, innerException) => MissingProperties = [];
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonRequiredConditionallyException"/> class.
+	/// </summary>
+	/// <param name="missingProperties">The JSON names of the properties that were required but absent.</param>
+	public JsonRequiredConditionallyException(IReadOnlyList<string> missingProperties)
+		: base(BuildMessage(missingProperties)) => MissingProperties = missingProperties;
+
+	/// <summary>
+	/// Gets the JSON names of the properties that were required but absent from the payload.
+	/// </summary>
+	public IReadOnlyList<string> MissingProperties { get; }
+
+	private static string BuildMessage(IReadOnlyList<string> missingProperties)
+	{
+		Ensure.NotNull(missingProperties);
+
+		string names = string.Join("', '", missingProperties);
+
+		return $"The following properties are required by their sibling values but were absent from the JSON payload: '{names}'.";
+	}
+}
